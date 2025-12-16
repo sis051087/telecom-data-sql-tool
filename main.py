d1427779909a82c74590121a2e2ea31e337f0c1c
Jim Chen <sis051087@gmail.com>
2025-12-16 18:17:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,17 @@ app = FastAPI()
 @app.get("/usage")
 def get_usage(year: int, operator: str):
     conn = sqlite3.connect("./data/mobile_usage.db")
-    df = pd.read_sql_query(f"SELECT * FROM mobile_usage.db WHERE year={year} AND operator='{operator}'", conn)
-    return df.to_dict()
+
+    df = pd.read_sql_query(
+        """
+        SELECT *
+        FROM mobile_usage
+        WHERE year = ?
+          AND operator = ?
+        """,
+        conn,
+        params=(year, operator)
+    )
+
+    conn.close()
+    return df.to_dict(orient="records")
