a2cec2939dfefbdbc9b1087bb83b79db00948f61
Jim Chen <sis051087@gmail.com>
2025-12-16 17:23:44 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+import sqlite3
+import pandas as pd
+
+app = FastAPI()
+
+@app.get("/usage")
+def get_usage(year: int, operator: str):
+    conn = sqlite3.connect("./data/telecom.db")
+    df = pd.read_sql_query(f"SELECT * FROM table WHERE year={year} AND operator='{operator}'", conn)
+    return df.to_dict()
